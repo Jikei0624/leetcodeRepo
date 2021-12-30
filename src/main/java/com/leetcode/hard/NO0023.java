97775f0c300702a1c4bcd46e9ce809faf2134c25
Jikei <zhuangziqi0624@gmail.com>
2021-12-30 11:39:03 +0800

    AC NO0007. Try NO0023.

---
+++ src/main/java/com/leetcode/hard/NO0023.java
@@ -0,0 +1,17 @@
+package com.leetcode.hard;
+
+public class NO0023 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        return null;
+    }
+}
